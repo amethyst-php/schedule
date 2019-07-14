1b2e7eea260a8d5e39c6211e52bb3e1bdd83b205
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/ScheduleManager.php
+++ src/Managers/ScheduleManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Schedule newEntity()
+ * @method \Amethyst\Schemas\ScheduleSchema getSchema()
+ * @method \Amethyst\Repositories\ScheduleRepository getRepository()
+ * @method \Amethyst\Serializers\ScheduleSerializer getSerializer()
+ * @method \Amethyst\Validators\ScheduleValidator getValidator()
+ * @method \Amethyst\Authorizers\ScheduleAuthorizer getAuthorizer()
+ */
 class ScheduleManager extends Manager
 {
     use ConfigurableManager;
